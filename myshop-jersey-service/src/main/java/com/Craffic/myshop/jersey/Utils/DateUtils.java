f6a6e2a32288a41039839b10809e8cbb08a61ac6
Craffic <392301161@qq.com>
2020-07-11 20:14:17 +0800

    java8 dateutils

--- myshop-jersey-service/src/main/java/com/Craffic/myshop/jersey/Utils/DateUtils.java
+++ myshop-jersey-service/src/main/java/com/Craffic/myshop/jersey/Utils/DateUtils.java
@@ -1,8 +1,17 @@
 package com.Craffic.myshop.jersey.Utils;
 
 import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 public class DateUtils {
+
+    public final static String DATE_PATTEN_1 = "yyyyMMdd";
+    public final static String DATE_PATTEN_2 = "yyyy-MM-dd";
+    public final static String DATE_PATTEN_3 = "yyyy/MM/dd";
 
     /**
      * 获取当前时间
@@ -36,11 +45,10 @@ public class DateUtils {
     public static Instant changeToInstant(int mode, long timeStamp){
         if (mode == 0){
             // 根据秒数转换时间
-            Instant instant = Instant.ofEpochSecond(timeStamp);
-            return instant;
+            return Instant.ofEpochSecond(timeStamp);
         } else if (mode == 1) {
             // 根据毫秒数转换时间
-            Instant instant = Instant.ofEpochMilli(timeStamp);
+            return Instant.ofEpochMilli(timeStamp);
         }
         return Instant.now();
     }
@@ -51,9 +59,85 @@ public class DateUtils {
      * @return
      */
     public static Instant convertStringToInstant(String strTime){
-//        2020-06-24T01:30:05+08:00[Asia/Shanghai]
-//        2020-06-24T01:30:05.928+08:00[Asia/Shanghai]
         Instant secondInstant = Instant.parse(strTime);
         return secondInstant;
+    }
+
+    /**
+     * 指定日期
+     */
+    public static LocalDate specifiedDate(int year, int month, int day){
+        LocalDate specifiedDate = LocalDate.of(year, month, day);
+        return specifiedDate;
+    }
+
+    /**
+     * 比较日期是否相等
+     */
+    public static Boolean compareDates(LocalDate localDate1, LocalDate localDate2){
+        return localDate1.isAfter(localDate2);
+    }
+
+    /**
+     * 格式化LocalDate
+     */
+    public static String formatDate(LocalDate date, String datePatten){
+        String formatDate = "";
+        if (DATE_PATTEN_1.equals(datePatten)){
+            formatDate = date.format(DateTimeFormatter.BASIC_ISO_DATE);
+        } else if (DATE_PATTEN_2.equals(datePatten)){
+            formatDate = date.format(DateTimeFormatter.ISO_DATE);
+        } else if (DATE_PATTEN_3.equals(datePatten)){
+            formatDate = date.format(DateTimeFormatter.ofPattern(datePatten));
+        }
+        return formatDate;
+    }
+
+    /**
+     * Date to Instant
+     * @param date
+     * @return
+     */
+    public static Instant dateConvertInstant(Date date){
+        Instant instant = date.toInstant();
+        return instant;
+    }
+
+    /**
+     * Date to LocalDate
+     */
+    public static LocalDate dateConvertLocalDate(Date date){
+        Instant instant = date.toInstant();
+        LocalDateTime localDateTime = LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
+        LocalDate localDate = localDateTime.toLocalDate();
+        return localDate;
+    }
+
+    /**
+     * Date to LocalDateTime
+     */
+    public static LocalDateTime dateConvertLocalDateTime(Date date){
+        Instant instant = date.toInstant();
+        LocalDateTime localDateTime = LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
+        return localDateTime;
+    }
+
+    /**
+     * LocalDateTime to Date
+     */
+    public static Date localDateTimeToDate(LocalDateTime localDateTime){
+        Instant instant = localDateTime.atZone(ZoneId.systemDefault()).toInstant();
+        Date date = Date.from(instant);
+        return date;
+    }
+
+    /**
+     * LocalDate to Date
+     */
+    public static Date localDateConvertDate(LocalDate localDate){
+        LocalDateTime localDateTime = localDate.atStartOfDay();
+        Instant instant = localDateTime.atZone(ZoneId.systemDefault()).toInstant();
+        Date date = Date.from(instant);
+        return date;
     }
 }
